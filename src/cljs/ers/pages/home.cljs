a9a4e9427b8c74f4cc5ad58ba21ddea19a3d0320
abby sassel <sassela@live.com>
2016-07-01 08:18:37 +0100

    [info retrieval] render result items

--- src/cljs/ers/pages/home.cljs
+++ src/cljs/ers/pages/home.cljs
@@ -13,9 +13,27 @@
                  :value     @input
                  :placeholder "search"}]
         [:button {:on-click (util/event-handler
-                              (fn [e]
-                                (dispatch [:items/search [@input]])))}
+                              (fn [e] (dispatch [:items/search [@input]])))}
          "SEARCH"]]])))
+
+
+(defn item-component
+  [{:keys [name link] :as item}]
+  [:div
+   [:h3 name]
+   [:button
+    {:on-click (util/event-handler
+                 (fn [e] (prn "YAY! " link)))}
+    "DOWNLOAD"]])
+
+
+(defn item-list
+  []
+  (let [items (subscribe [:items/list-items])]
+    (fn []
+      [:div
+       [:h2 (str (count @items) " results:")]
+       (into [:div] (map item-component @items))])))
 
 
 (defn page []
@@ -25,5 +43,6 @@
      [:button {:on-click #(dispatch [:print-db])} "DB"]
      [:button {:on-click #(dispatch [:items/get-all])} "ALL ITEMS"]
      [search]
+     [item-list]
      [:a {:href "#/about"} "about page"]
      ]))
